7ab535ea6afc3a5f1185c8ff6befe1f082217046
begoña <begoa@bego-vaio>
2017-11-21 00:18:32 +0100

    primer programa

---
+++ Prueba2/src/prueba2/Prueba2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba2;
+
+/**
+ *
+ * @author begoña
+ */
+public class Prueba2 {
+
+    private int hora,minuto,segundo;
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+                
+                
+    }
+    
+}
